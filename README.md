3d9e34655ff19ac959cb219aa53360f0b16a2552
Jake Walker <hi@jakew.me>
2021-05-02 12:13:14 +0100

    Add Betaflight Configurator

---
+++ README.md
@@ -0,0 +1,19 @@
+# Chocolatey Packages
+
+This repository contains multiple Chocolatey packages, take a look at one for more specifics:
+
+- [Betaflight Configurator](./betaflight-configurator)
+
+## Installation
+
+Use Chocolatey to install any of these packages. Installation instructions for Chcolatey are [here](https://chocolatey.org/install).
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update GitHub Actions Workflows and tests as appropriate.
+
+## License
+
+[MIT](./LICENSE)
---
+++ betaflight-configurator/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/betaflight/betaflight-configurator/releases/download/10.7.0/betaflight-configurator-installer_10.7.0_win32.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Betaflight Configurator*'
+
+  checksum      = '7A233DCE35FD1166C5DA484BAA0A50C9A4F883C1EF21BBC531844A7F1D626693'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
